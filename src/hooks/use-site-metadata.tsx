b42babde4ab3c89c5604c8dc616d4cf54fab6ed7
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2026-04-22 12:42:11 +0700

    Update site URL in use-site-metadata hook

--- src/hooks/use-site-metadata.tsx
+++ src/hooks/use-site-metadata.tsx
@@ -3,7 +3,7 @@ const useSiteMetadata = () => {
     siteTitle: `Vite Emilia`,
     siteTitleAlt: `Vite Emilia | AREA44`,
     siteHeadline: `Vite Emilia | AREA44`,
-    siteUrl: `https://starter-emilia.netlify.app`,
+    siteUrl: `https://vite-emilia.onrender.com`,
     siteDescription:
       "Minimalistic portfolio/photography site with masonry grid, page transitions and big images.",
     siteImage: `/og-image.jpg`,

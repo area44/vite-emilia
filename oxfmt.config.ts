535ef533083d1bf08c5e66a69e076d36fafd4e5b
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2026-05-05 23:51:10 +0700

    setup

---
+++ oxfmt.config.ts
@@ -0,0 +1,6 @@
+import oxfmtConfig from "@area44/oxfmt-config";
+import { defineConfig } from "oxfmt";
+
+export default defineConfig({
+  extends: [oxfmtConfig],
+});
---
+++ oxlint.config.ts
@@ -0,0 +1,6 @@
+import oxlintConfig from "@area44/oxlint-config";
+import { defineConfig } from "oxlint";
+
+export default defineConfig({
+  extends: [oxlintConfig],
+});

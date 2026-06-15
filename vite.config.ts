46b66d072a61398fa99d3d71a635efd38d565eb5
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2026-06-15 14:24:28 +0700

    refactor: tanstack (#94)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
       ],
     },
     sortTailwindcss: {
-      stylesheet: "./src/index.css",
+      stylesheet: "./src/styles/globals.css",
       attributes: ["class", "className"],
       functions: ["clsx", "cn", "cva", "tv"],
     },
